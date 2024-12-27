ba4ea6f5e03c34a2fe859336b09630f9aa7c154b
jingxiang0405 <shane200445@gmail.com>
2024-12-28 00:27:56 +0800

    [Create][Update] Create src/services whcih help usere grab json data. Complete feature of map navigation.

--- src/components/TaiwanMap.vue
+++ src/components/TaiwanMap.vue
@@ -1,290 +1,274 @@
 <template>
-    <div ref="mapContainer" style="width: 100%; height: 600px;"></div>
+
+    <div ref="mapContainer" class="map-container">
+        <svg ref="canvas" class="canvas" style="width: inherit; height: inherit;">
+            <g ref="map">
+
+            </g>
+        </svg>
+
+    </div>
+    <div class="tooltip"></div>
+
 </template>
 
 <script>
 import * as d3 from 'd3';
 import * as topojson from 'topojson-client';
-import { watch } from 'vue';
+import bboxClip from '@turf/bbox-clip';
+import TopoDataService from '../services/map/TopoDataService';
 
 export default {
     name: 'TaiwanMap',
     data() {
         return {
-            geoJsonUrl: 'https://raw.githubusercontent.com/Zovjsra/taiwan-map/main/COUNTY_MOI_1090820.json',
-            topoJsonData: null,
-            currenetTopoJsonData: null,
-            countyMap: {
-                "臺北市": {
-                    "path": "/Taipei",
-                    "textDisplay": {
-                        "offsetX": -50,
-                        "offsetY": -20
-                    },
-                    "lineDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 3
-                    }
+            geoData: {
+                "Taiwan": {
+                    topo: null
                 },
-                "新北市": {
-                    "path": "/NewTaipei",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 25
-                    }
-                },
-                "桃園市": {
-                    "path": "/Taoyuan",
-                    "textDisplay": {
-                        "offsetX": -15,
-                        "offsetY": -15
-                    }
-                },
-                "臺中市": {
-                    "path": "/Taichung",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 10
-                    }
-                },
-                "臺南市": {
-                    "path": "/Tainan",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "高雄市": {
-                    "path": "/Kaohsiung",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "基隆市": {
-                    "path": "/Keelung",
-                    "textDisplay": {
-                        "offsetX": 20,
-                        "offsetY": -20
-                    },
-                    "lineDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "新竹市": {
-                    "path": "/Hsinchu",
-                    "textDisplay": {
-                        "offsetX": -40,
-                        "offsetY": 0
-                    },
-                    "lineDisplay": {
-                        "offsetX": 18,
-                        "offsetY": -5
-                    }
-                },
-                "新竹縣": {
-                    "path": "/HsinchuCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "苗栗縣": {
-                    "path": "/MiaoliCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "彰化縣": {
-                    "path": "/ChanghuaCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "南投縣": {
-                    "path": "/NantouCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "雲林縣": {
-                    "path": "/YunlinCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "嘉義市": {
-                    "path": "/Chiayi",
-                    "textDisplay": {
-                        "offsetX": -100,
-                        "offsetY": 10
-                    },
-                    "lineDisplay": {
-                        "offsetX": 20,
-                        "offsetY": -4
-                    }
-                },
-                "嘉義縣": {
-                    "path": "/ChiayiCounty",
-                    "textDisplay": {
-                        "offsetX": 30,
-                        "offsetY": 0
-                    },
 
-                },
-                "屏東縣": {
-                    "path": "/PingtungCounty",
-                    "textDisplay": {
-                        "offsetX": -10,
-                        "offsetY": 0
-                    }
-                },
-                "宜蘭縣": {
-                    "path": "/YilanCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "花蓮縣": {
-                    "path": "/HualienCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "臺東縣": {
-                    "path": "/TaitungCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
-                },
-                "澎湖縣": {
-                    "path": "/PenghuCounty",
-                    "textDisplay": {
-                        "offsetX": -40,
-                        "offsetY": 0
-                    }
-                },
-                "金門縣": {
-                    "path": "/KinmenCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": -20
-                    }
-                },
-                "連江縣": {
-                    "path": "/LienchiangCounty",
-                    "textDisplay": {
-                        "offsetX": 0,
-                        "offsetY": 0
-                    }
+                "TaipeiCity": {
+                    topo: null
                 }
-            }
+            },
+            currentFocusCounty: null, // A string of county/city name
+            currentSelectedDistrictID: null
         }
     },
     mounted() {
-        this.fetchData();
+        this.initGeoTaiwan();
+        window.addEventListener('resize', this.handleResize);
+        this.initGeoTaipei();
     },
     methods: {
-        async fetchData() {
-            // fetch GeoJSON data
-            try {
-                const response = await fetch(this.geoJsonUrl);
-                const geoJsonData = await response.json();
+        handleResize() {
+            d3.select('.tooltip').style('visibility', 'hidden');
+            console.log("resize")
+            this.drawMap();
 
-                this.topoJsonData = topojson.feature(geoJsonData, geoJsonData.objects.COUNTY_MOI_1090820);
-                this.currenetTopoJsonData = this.topoJsonData;
+        },
+        initGeoTaiwan() {
+            const topoTaiwan = TopoDataService.topoTaiwan;
+            let geoTaiwan = topojson.feature(topoTaiwan, topoTaiwan.objects.COUNTY_MOI_1090820);
 
-            } catch (error) {
-                console.error('Error fetching the GeoJSON data:', error);
+            const bbox = [119.3, 20.9, 124.6, 25.4];
+            for (let i = 0; i < geoTaiwan.features.length; i++) {
+                geoTaiwan.features[i] = bboxClip(geoTaiwan.features[i], bbox)
             }
 
-            console.log('topoJsonData:', this.topoJsonData);
+
+            this.geoData["Taiwan"].topo = geoTaiwan;
+
 
             this.drawMap();
         },
-        filterCounty(countyName) {
-            this.currenetTopoJsonData = this.topoJsonData.features.filter((d) => d.properties.COUNTYNAME === countyName);
+        async initGeoTaipei() {
+            const topoTaipei = TopoDataService.topoTaipei;
+            console.log(topoTaipei)
+            let geoTaipei = topojson.feature(topoTaipei, topoTaipei.objects.geometry);
+
+            this.geoData["TaipeiCity"].topo = geoTaipei;
+            console.log("taipei loaded:", this.geoData["TaipeiCity"].topo);
         },
+
         drawMap() {
-            const width = 1200;
-            const height = 700;
+            console.log("drawing map");
+            // cleanup
+            const container = this.$refs.mapContainer;
+            const width = container.offsetWidth;
+            const height = container.offsetHeight;
+            const svg = d3.select(this.$refs.canvas);
+            const map = d3.select(this.$refs.map);
 
             // Set up the projection and path generator
             const projection = d3.geoMercator()
                 .center([121, 23.5]) // Centered around Taiwan
-                .scale(10000)
-                .translate([width / 2, height / 2]);
+                .scale(width * 17.5)
+                .translate([width / 2, height / 2 + 10]);
 
             const path = d3.geoPath().projection(projection);
 
-            // Create an SVG container
-            const svg = d3.select(this.$refs.mapContainer)
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height);
+            // for zooming
+            const initialTransform = d3.zoomIdentity.scale(1);
+            const zoom = d3.zoom()
+                .on("zoom", (event) => {
+                    map.attr("transform", event.transform);
 
-            // Draw the map: Taiwan's counties
-            svg.selectAll('path')
-                .data(this.topoJsonData.features)
+                });
+
+            // setup tooltip
+            const tooltip = d3.select(".tooltip");
+
+            map.on('mousemove', function (event, d) {
+                tooltip.style('left', `${event.pageX}px`)
+                    .style('top', `${event.pageY - 30}px`);
+            })
+
+            // Draw the map
+            const self = this;
+            map.selectAll('.geo-path')
+                .data(self.geoData["Taiwan"].topo.features)
                 .enter().append('path')
                 .attr('d', path)
+                .attr('class', 'geo-path')
                 .attr('fill', '#ccc')
-                .attr('stroke', 'blue')
-                .attr('stroke-width', 1)
-                .on('mouseover', function (event) {
-                    d3.select(this).attr('fill', '#ffcc00'); // Highlight on hover
+                .attr('stroke', 'black')
+                .attr('stroke-width', 0.5)
+                .attr("pointer-events", "all")
+                .on('mouseover', function (event, d) {
+                    const tooltip = d3.select('.tooltip');
+                    tooltip
+                        .style('visibility', 'visible')
+                        .style('left', `${event.pageX + 10}px`)
+                        .style('top', `${event.pageY + 10}px`)
+                        .text(d.properties.COUNTYNAME);
+
+                    d3.select(this).attr('fill', '#ffcc00');
+
                 })
                 .on('mouseout', function (event) {
-                    d3.select(this).attr('fill', '#ccc'); // Reset color after hover
+
+                    tooltip.style('visibility', 'hidden');
+                    d3.select(this).attr('fill', '#ccc');
                 })
                 .on('click', function (event, d) {
-                    console.log('Clicked on:', d.properties.COUNTYNAME);
-                });
-
-            // Add county names and lines
-            const self = this; // Store reference to Vue component instance
-            svg.selectAll('text')
-                .data(this.topoJsonData.features)
-                .enter().append('g')
-                .each(function (d) {
-                    const centroid = path.centroid(d);
-                    const countyData = self.countyMap[d.properties.COUNTYNAME];
-                    const offsetX = parseInt(countyData.textDisplay.offsetX);
-                    const offsetY = parseInt(countyData.textDisplay.offsetY);
-                    const displayLine = countyData.lineDisplay;
 
 
-                    if (displayLine) {
-                        const lineOffsetX = parseInt(displayLine.offsetX);
-                        const lineOffsetY = parseInt(displayLine.offsetY);
-                        d3.select(this).append('line')
-                            .attr('x1', centroid[0])
-                            .attr('y1', centroid[1])
-                            .attr('x2', centroid[0] + offsetX + lineOffsetX)
-                            .attr('y2', centroid[1] + offsetY + lineOffsetY)
-                            .attr('stroke', 'black')
-                            .attr('stroke-width', 1);
-                    }
+                    d3.select(this)
+                        .attr('fill', '#ccc')
+                        .on("mouseover", null)
+                        .on("mousemove", null)
+                        .on("mouseout", null)
+                    d3.selectAll('.geo-path').attr('fill', '#ccc')
+                        .on("mouseover", null)
+                        .on("mousemove", null)
+                        .on("mouseout", null)
 
-                    d3.select(this).append('text')
-                        .attr('x', centroid[0] + offsetX)
-                        .attr('y', centroid[1] + offsetY)
-                        .text(d.properties.COUNTYNAME)
-                        .attr('font-size', '12px')
-                        .attr('fill', 'black')
-                        .attr('text-anchor', 'middle');
-                });
+                    self.currentFocusCounty = d.properties.COUNTYENG;
+
+                    // draw detailed district (use Taipei for testing)
+                    map.selectAll(".district-path")
+                        .data(self.geoData["TaipeiCity"].topo.features)
+                        .enter()
+                        .append("path")
+                        .attr("d", path)
+                        .attr("class", d => `district-path id-${d.properties.ID}`)
+                        .attr("fill", "none")
+                        .attr("stroke", "pink")
+                        .attr("stroke-width", 0.75)
+
+
+                    // for interaction
+                    map.selectAll(".interaction-circle")
+                        .data(self.geoData["TaipeiCity"].topo.features)
+                        .enter()
+                        .append("circle")
+                        .attr("class", "interaction-circle")
+                        .attr("cx", d => {
+                            const [x] = path.centroid(d);
+                            return x;
+                        })
+                        .attr("cy", d => {
+                            const [, y] = path.centroid(d);
+                            return y;
+                        })
+                        .attr("r", "0.25vh")
+                        .attr("fill", "none")
+                        .attr("pointer-events", "all")
+                        .on("mouseover", (event, d) => {
+                            console.log("d=", d.properties.ID)
+                            tooltip
+                                .style('visibility', 'visible')
+                                .style('left', `${event.pageX + 10}px`)
+                                .style('top', `${event.pageY + 10}px`)
+                                .text(d.properties.DISTRICT);
+                            self.currentSelectedDistrictID = d.properties.ID;
+                            d3.select(`.district-path.id-${d.properties.ID}`).attr('fill', '#ffcc00');
+
+                        })
+                        .on('mouseout', function () {
+
+                            tooltip.style('visibility', 'hidden');
+                            d3.select(`.district-path.id-${self.currentSelectedDistrictID}`).attr('fill', '#ccc');
+                        })
+
+                    // zoom in
+                    const bounds = path.bounds(d);
+                    const dx = bounds[1][0] - bounds[0][0];
+                    const dy = bounds[1][1] - bounds[0][1];
+                    const x = (bounds[0][0] + bounds[1][0]) / 2;
+                    const y = (bounds[0][1] + bounds[1][1]) / 2;
+
+                    const scale = Math.min(width / dx, height / dy) * 0.9;
+                    const translate = [width / 2 - scale * x, height / 2 - scale * y];
+                    map.transition().duration(500).call(zoom.transform, d3.zoomIdentity.translate(translate[0], translate[1]).scale(scale));
+
+
+                })
+
+            // Click event for zooming back to original view
+            svg.on("click", function (event) {
+                if (!event.target.closest('path')) {
+                    map.selectAll('.district-path').remove()
+                    map.transition().duration(750).call(zoom.transform, initialTransform);
+                    self.currentFocusCounty = null;
+
+                    // Reset tooltip
+                    map.selectAll(".geo-path")
+                        .attr("stroke-width", 0.5)
+                        .on('mouseover', function (event, d) {
+                            tooltip
+                                .style('visibility', 'visible')
+                                .style('left', `${event.pageX + 10}px`)
+                                .style('top', `${event.pageY + 10}px`)
+                                .text(d.properties.COUNTYNAME);
+
+                            d3.select(this).attr('fill', '#ffcc00');
+
+                        }).on('mousemove', function (event, d) {
+
+                            // Update tooltip position
+                            tooltip
+                                .style('left', `${event.pageX}px`)
+                                .style('top', `${event.pageY - 30}px`);
+                        })
+                        .on('mouseout', function (event) {
+
+                            tooltip.style('visibility', 'hidden');
+                            d3.select(this).attr('fill', '#ccc');
+                        })
+                }
+            });
+
         },
 
-        
-    }
-};
+    },
+
+
+
+}
 </script>
+
+<style scoped>
+.tooltip {
+    position: absolute;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: white;
+    padding: 5px 10px;
+    border-radius: 5px;
+    font-size: 20px;
+    pointer-events: none;
+    visibility: hidden;
+    white-space: nowrap;
+    z-index: 1000;
+}
+
+.map-container {
+    width: 50vw;
+    height: 100vh;
+    border: thick double black;
+    margin: 1vh;
+    border-width: 2px;
+    border-color: black;
+}
+</style>
